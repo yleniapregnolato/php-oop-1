0d86c6d7cb4cee0e65a0120ffd7d83e2c63e4c4d
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-31 17:20:14 +0200

    add: set and get in genre.php

---
+++ genre.php
@@ -0,0 +1,22 @@
+<?php
+
+class Genre {
+    private string $genre;
+
+    // costruttore
+    public function __construct(string $nome) {
+        $this->genre = $genre;
+    }
+    
+    // set
+    public function setGenre(string $genre) {
+        $this->genre = $genre;
+    }
+
+    // get controlla il dato in uscira
+    public function getGenre() {
+
+        return $this-> $genre;
+    }
+
+}
