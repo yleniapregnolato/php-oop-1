26ade93f74b3c6423dacbdf238a0aa16c002c270
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-31 18:28:31 +0200

    add: movie's instances in index.php

--- index.php
+++ index.php
@@ -6,9 +6,14 @@ require_once "movie.php";
 
 // creo le istanze della classe genre
 $genre1 = new Genre("Commedia");
-$genre2 = new Genre("Triller");
+$genre2 = new Genre("Thriller");
 $genre3 = new Genre("Animazione");
 $genre4 = new Genre("Azione");
 
+// creo le istanze della classe movie
+$movie1 = new Movie("Interstellar", 2014, $genre4);
+$movie2 = new Movie("Una notte al museo", 2006, $genre1);
+$movie3 = new Movie("I guardiano del destino", 2011, $genre2);
+$movie4 = new Movie("Mulan", 1998, $genre3);
 
 <?
