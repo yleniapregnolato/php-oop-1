4479715e81292a02bdb2e7db9da59f9deddc895d
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-31 17:52:33 +0200

    add: construct in movie.php

--- movie.php
+++ movie.php
@@ -3,5 +3,16 @@ class Movie {
     private string $title;
     private int $year;
     private Genre $genre;
+
+    // costruttore
+    public function __construct(string $title, int $year, Genre $genre) {
+        $this -> Titolo = $title;
+        $this -> Anno = $year;
+        $this -> Genere = $genre;
+    }
+
+    
 }
+
+
 ?>
