682da3c56e725c9d34f8441d78027b44c4415c2a
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-31 17:34:33 +0200

    add: instances in index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 // importo la classe genere
 require_once "genre.php";
+
+// creo le istanze della classe genre
+$genre1 = new Genre("Commedia");
+$genre2 = new Genre("Triller");
+$genre3 = new Genre("Animazione");
+$genre4 = new Genre("Azione");
+
 <?
